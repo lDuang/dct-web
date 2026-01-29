7a9134a9c844c3d7d1c588e3f2c0282acf8b922f
wangxu <78087597+lDuang@users.noreply.github.com>
2026-01-29 10:44:52 +0000

    refactor-项目配置更新,支持内联样式

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
+  server: {
+    allowedHosts: ['5173.app.cloudstudio.work', 'localhost', '127.0.0.1']
+  }
 })
